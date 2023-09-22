fe943e9d6d6e38745608eff6564c7d72cc87a23e
Marián <Matiasus@users.noreply.github.com>
2023-09-22 13:24:58 +0200

    Added - Power up sequence

--- src/sd.c
+++ src/sd.c
@@ -1,6 +1,6 @@
 /**
  * --------------------------------------------------------------------------------------+
- * @brief       SD CARD INTERFACING
+ * @brief       SD / uSD CARD 
  * --------------------------------------------------------------------------------------+
  *              Copyright (C) 2023 Marian Hrinko.
  *              Written by Marian Hrinko (mato.hrinko@gmail.com)
@@ -21,13 +21,57 @@
 
 // INCLUDE libraries
 // ------------------------------------------------------------------
+#include <delay.h>
 #include "sd.h"
 
-const uint8_t SD_SEQUENCE[] PROGMEM = {
-   x
-  ,SD_CMD0,   0, 95
-  ,SD_ACMD41, 0, xx
-  ,SD_CMD8,   0, xx
+/**
+ * @brief   SD Card Power Up Sequence
+ *
+ * @param   void
+ *
+ * @return  void
+ */
+void SD_PowerUp (void)
+{
+  uint8_t i = 10;
+  
+  // Power Up Time Delay
+  // ----------------------------------------------------------------
+  _delay_ms(250);                                       // power up time
+
+  // Supply Ram Up Sequence
+  // ----------------------------------------------------------------
+  CS_DISABLE();                                         // hold CS high
+  _delay_ms(1);                                         // supply ramp up time 
+  while (i--) {                                         // supply ramp up cycles
+    SPI_WriteByte (0xff);                               // min 74 cycles
+  }
+  // Deselect Card
+  // accor. http://www.rjhcoding.com/avrc-sd-interface-1.php
+  // ----------------------------------------------------------------
+  CS_DISABLE();
+  SPI_WriteByte (0xff);
+}
+
+/**
+ * @brief   SD Card Power Up Sequence
+ *
+ * @param   void
+ *
+ * @return  uint8_t
+ */
+uint8_t SD_IdleState (void)
+{
+  uint8_t response = 0xff;
+  
+  CS_ENABLE ();                                         // CS low
+  SD_SendCommand (SD_CMD0, SD_CMD0_ARG, SD_CMD0_CRC);   // Send CMD0
+  if (SD_GetResponseR1 != 0x01) {
+    return SD_GetResponseR1;
+  }  
+  CS_DISABLE ();                                        // CS High
+  
+  return response;
 }
 
 /**
@@ -35,11 +79,15 @@ const uint8_t SD_SEQUENCE[] PROGMEM = {
  *
  * @param   void
  *
- * @return  void
+ * @return  uint8_t
  */
-void SD_Init (void)
+uint8_t SD_Init (void)
 {
-  SD_SendCommand (SD_CMD0, SD_CMD0_ARG0, CRC);
+  SPI_PortInit ();                                      // SPI ports init
+  SPI_SlowSpeedInit ();                                 // SPI slow speed 250kHz
+  SD_PowerUp ();                                        // Power up sequence
+  
+  return 0;
 }
 
 /**
@@ -52,6 +100,27 @@ void SD_Init (void)
  * @return  void
  */
 void SD_SendCommand (uint8_t command, uint32_t argument, uint8_t crc)
+{ 
+  SPI_WriteByte (command);                              // send command
+  SPI_WriteByte ((uint8_t) argument >> 24);             // send arguments
+  SPI_WriteByte ((uint8_t) argument >> 16);             // 
+  SPI_WriteByte ((uint8_t) argument >>  8);             // 
+  SPI_WriteByte ((uint8_t) argument >>  0);             // 
+  SPI_WriteByte (crc | 0x01);                           // cyclic redundancy check
+}
+
+/**
+ * @brief   SD Card Response R1
+ *
+ * @param   void
+ *
+ * @return  uint8_t
+ */
+void SD_GetResponseR1 (void)
 {
+  uint8_t response = 0xff;
   
+  response = SPI_ReadByte ();
+  
+  return response;
 }
