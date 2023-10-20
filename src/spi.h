d13e00cf1d540c59a98e0ec45221ae5fda5fb68a
Marián <Matiasus@users.noreply.github.com>
2023-10-20 23:19:05 +0200

    BIG ISSUE - macro CS_ENABLE(), added '~' before brackets

--- src/spi.h
+++ src/spi.h
@@ -22,7 +22,7 @@
 #ifndef __SPI_H__
 #define __SPI_H__
 
-  // INCLUDE libraries
+  // includes
   #include <avr/io.h>
 
   // atmega328p
@@ -30,10 +30,10 @@
 
     #define SPI_DDR           DDRB
     #define SPI_PORT          PORTB
-    #define SPI_SCK           5     // or SCLK
-    #define SPI_MISO          4     // or DO
-    #define SPI_MOSI          3     // or SDA, SDI, DI
-    #define SPI_SS            1     // or CS
+    #define SPI_SCK           PINB5     // or SCLK
+    #define SPI_MISO          PINB4     // or DO
+    #define SPI_MOSI          PINB3     // or SDA, SDI, DI
+    #define SPI_SS            PINB2     // or CS
 
     // SPI registers
     #define SPI_SPSR          SPSR
@@ -41,9 +41,9 @@
     #define SPI_SPDR          SPDR
 
   #endif
-  
+
   // macros
-  #define CS_ENABLE()         SPI_PORT &= (1 << SPI_SS)
+  #define CS_ENABLE()         SPI_PORT &= ~(1 << SPI_SS)
   #define CS_DISABLE()        SPI_PORT |= (1 << SPI_SS)
 
   /**
@@ -65,48 +65,12 @@
   void SPI_SlowSpeedInit (void);
 
   /**
-   * @desc    SPI Fast Speed Init
-   *
-   * @param   void
-   *
-   * @return  void
-   */
-  void SPI_FastSpeedInit (void);
-
-  /**
    * @desc    SPI Write Byte
    *
    * @param   uint8_t
    *
-   * @return  void
-   */
-  void SPI_WriteByte (uint8_t);
-
-  /**
-   * @desc    SPI Write Word / big endian (MSB first)
-   *
-   * @param   uint16_t
-   *
-   * @return  void
-   */
-  void SPI_WriteWord (uint16_t);
-
-  /**
-   * @desc    SPI Read Byte
-   *
-   * @param   void
-   *
    * @return  uint8_t
    */
-  uint8_t SPI_ReadByte (void);
-
-  /**
-   * @desc    SPI Write / Read Byte
-   *
-   * @param   uint8_t
-   *
-   * @return  uint8_t
-   */
-   uint8_t SPI_WriteReadByte (uint8_t);
-
+  uint8_t SPI_Transfer (uint8_t);
+  
 #endif
