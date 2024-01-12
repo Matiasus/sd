9903760616cd259fecd5651525f3c6c89f0b0b60
Marián <Matiasus@users.noreply.github.com>
2024-01-12 13:52:24 +0100

    Add structures

--- src/fat32.h
+++ src/fat32.h
@@ -22,46 +22,62 @@
 #ifndef __FAT32_H__
 #define __FAT32_H__
 
-  // Partition Type used in the partition record 
+  // Partition Type used in the partition record
+  //
   // @src https://d1.amobbs.com/bbs_upload782111/files_2/armok0150242.pdf
+  //      https://www.pjrc.com/tech/8051/ide/fat32.html
+  //      https://www.richud.com/wiki/FAT32_Filesystem_Slice_Design_Card
+  //      https://en.wikipedia.org/wiki/Master_boot_record#PTE
   // --------------------------------------------------------------------------------------
-  #define PART_TYPE_UNKNOWN           0x00
-  #define PART_TYPE_FAT12             0x01
-  #define PART_TYPE_XENIX             0x02
-  #define PART_TYPE_DOSFAT16          0x04
-  #define PART_TYPE_EXTDOS            0x05
-  #define PART_TYPE_FAT16             0x06 
-  #define PART_TYPE_NTFS              0x07
-  #define PART_TYPE_FAT32             0x0B
-  #define PART_TYPE_FAT32LBA          0x0C
-  #define PART_TYPE_FAT16LBA          0x0E
-  #define PART_TYPE_EXTDOSLBA         0x0F
-  #define PART_TYPE_ONTRACK           0x33
-  #define PART_TYPE_NOVELL            0x40
-  #define PART_TYPE_PCIX              0x4B
-  #define PART_TYPE_PHOENIXSAVE       0xA0
-  #define PART_TYPE_CPM               0xDB
-  #define PART_TYPE_DBFS              0xE0
-  #define PART_TYPE_BBT               0xFF
+  #define PE_TYPECODE_UNKNOWN           0x00
+  #define PE_TYPECODE_FAT12             0x01
+  #define PE_TYPECODE_XENIX             0x02
+  #define PE_TYPECODE_DOSFAT16          0x04
+  #define PE_TYPECODE_EXTDOS            0x05
+  #define PE_TYPECODE_FAT16             0x06 
+  #define PE_TYPECODE_NTFS              0x07
+  #define PE_TYPECODE_FAT32             0x0B
+  #define PE_TYPECODE_FAT32LBA          0x0C
+  #define PE_TYPECODE_FAT16LBA          0x0E
+  #define PE_TYPECODE_EXTDOSLBA         0x0F
+  #define PE_TYPECODE_ONTRACK           0x33
+  #define PE_TYPECODE_NOVELL            0x40
+  #define PE_TYPECODE_PCIX              0x4B
+  #define PE_TYPECODE_PHOENIXSAVE       0xA0
+  #define PE_TYPECODE_CPM               0xDB
+  #define PE_TYPECODE_DBFS              0xE0
+  #define PE_TYPECODE_BBT               0xFF
 
   // MASTER BOOT RECORD
   typedef struct MBR {
     uint8_t bootstrap[446];
-    uint8_t partition1[16];
-    uint8_t partition2[16];
-    uint8_t partition3[16];
-    uint8_t partition4[16];
-    uint8_t signature[2];
+    PE partition1;                                      // 16 bytes Partition Entry
+    PE partition2;                                      // 16 bytes Partition Entry
+    PE partition3;                                      // 16 bytes Partition Entry
+    PE partition4;                                      // 16 bytes Partition Entry
+    uint8_t signature[2];                               // signature => must be 0xAA55
   } __attribute__((packed)) MBR;
   
   // PARTITION ENTRY
   typedef struct PE {
     uint8_t status[1];                                  // status: 0x80-active, 0x00-inactive
-    uint8_t chsBegin[3];
-    uint8_t typeCode[1];                                // partition type: 0x0E-FAT16 LBA, 0x0C-FAT32 LBA
-    uint8_t chsEnd[3];
-    uint8_t lbaBegin[4];                                // LBA of first sector
+    uint8_t CHSBegin[3];                                // Cylinder/Head/Sector start
+    uint8_t typeCode[1];                                // partition type: 0x0E-FAT16 LBA, 0x0B-FAT32, 0x0C-FAT32 LBA
+    uint8_t CHSEnd[3];                                  // Cylinder/Head/Sector end
+    uint8_t LBABegin[4];                                // LBA of first sector
     uint8_t noOfSectors[4];                             // numbers of sectors in partition
   } __attribute__((packed)) PE;
+
+  // BOOT SECTOR
+  typedef struct BS {
+    uint8_t jumpCode[3];
+    uint8_t oemName[8];
+    uint8_t bytesPerSector[2];
+    uint8_t sectorsPerCluster[1];
+    uint8_t reservedSectors[2];
+    uint8_t numOfFat[1];
+    uint8_t empty[509];
+    uint8_t signature[2];                               // signature => must be 0xAA55
+  } __attribute__((packed)) BS;
 
 #endif
