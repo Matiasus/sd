be004fb85fbc1b61926d458eb2ace5b7e806d485
Marián <Matiasus@users.noreply.github.com>
2024-01-16 20:47:34 +0100

    Testing with success

--- src/fat32.c
+++ src/fat32.c
@@ -25,6 +25,28 @@
 #include "fat32.h"
 
 /**
+ * @brief   FAT32 Init
+ *
+ * @param   void
+ *
+ * @return  uint8_t
+ */
+uint8_t FAT32_Init (void)
+{
+  // init SD structure
+  // -------------------------------------------------------------------------------------  
+  SD sd = { .voltage = 0, .sdhc = 0, .version = 0 };
+
+  // init SD Card
+  // -------------------------------------------------------------------------------------
+  if (SD_Init (&sd) == SD_ERROR) {
+    return FAT32_ERROR;
+  }
+
+  return FAT32_SUCCESS;
+}
+
+/**
  * @brief   Read Master Boot Record
  *
  * @param   void
@@ -33,15 +55,23 @@
  */
 uint8_t FAT32_Master_Boot_Record (void)
 {
+  char str[10];
   uint8_t buffer[512];
+  uint16_t signature;
   uint32_t lba_begin;
-  uint32_t sectors;
 
   // Read MBR
   // ----------------------------------------------------------------
   SD_Read_Block (0, buffer);
   s_MBR * MBR = (s_MBR *) buffer;
 
+  signature = (MBR->signature[0] << 8) | (MBR->signature[1]);                 // signature
+
+  // Check signature 0x55AA
+  // ----------------------------------------------------------------
+  if (signature != MBR_SIGNATURE) {
+    return FAT32_ERROR;
+  }
   // Only 0x80 or 0x00 status accepted
   // ----------------------------------------------------------------
   if (MBR->partition1.status & PE_STATUS_ACTIVE_FLAG) {
@@ -49,17 +79,31 @@ uint8_t FAT32_Master_Boot_Record (void)
   }
   // Only FAT32 or FAT32LBA type code accepted
   // ----------------------------------------------------------------
-  if (MBR->partition1->typeCode != PE_TYPECODE_FAT32 ||
-      MBR->partition1->typeCode != PE_TYPECODE_FAT32LBA) {
+  if ((MBR->partition1.typeCode != PE_TYPECODE_FAT32) &&
+      (MBR->partition1.typeCode != PE_TYPECODE_FAT32LBA)) {
     return FAT32_ERROR;
   }
   // Calculate LBA begin address
   // ----------------------------------------------------------------
-  lba_begin = FAT32_Get_Uint32_LE (MBR->partition1.LBABegin) * BYTES_PER_SECTOR;
+  uint32_t lba;
+  lba = ((((unsigned long) MBR->partition1.LBABegin[3])<<24) & 0xFF000000) |
+        ((((unsigned long) MBR->partition1.LBABegin[2])<<16) & 0x00FF0000) |
+        ((((unsigned long) MBR->partition1.LBABegin[1])<< 8) & 0x0000FF00) |
+        ((((unsigned long) MBR->partition1.LBABegin[0])<< 0) & 0x000000FF) ;
+
+  SSD1306_SetPosition (2, 2);
+  SSD1306_DrawString ("TYPE: 0x", NORMAL);
+  sprintf (str, "%02x", MBR->partition1.typeCode);
+  SSD1306_DrawString (str, NORMAL);
+
+  SSD1306_SetPosition (2, 3);
+  SSD1306_DrawString ("LBA:  0x", NORMAL);
+  sprintf (str, "%08x", (unsigned int) lba);
+  SSD1306_DrawString (str, NORMAL);
 
   // Read Volume ID or Boot Sector or Block Parameter Bios
   // ----------------------------------------------------------------
-  SD_Read_Block (lba_begin, buffer);
+  SD_Read_Block (lba, buffer);
   s_VID * VID = (s_VID *) buffer;
 
   return FAT32_SUCCESS;
@@ -74,6 +118,9 @@ uint8_t FAT32_Master_Boot_Record (void)
  */
 uint8_t FAT32_Get_Uint32_LE (uint8_t * n)
 {
-  uint32_t number = n[3] << 24 & 0xFF000000 | n[2] << 16 & 0x00FF0000 | n[1] << 8 & 0x0000FF00 | n[0] << 0 & 0x000000FF;
+  uint32_t number = (((unsigned long) n[3] << 24) & 0xFF000000) |
+                    (((unsigned long) n[2] << 16) & 0x00FF0000) | 
+                    (((unsigned long) n[1] <<  8) & 0x0000FF00) | 
+                    (((unsigned long) n[0] <<  0) & 0x000000FF) ;
   return number;
 }
