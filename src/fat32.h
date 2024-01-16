fb25fa45ca80e3acf45b13626836c0e6f945d26a
Marián <Matiasus@users.noreply.github.com>
2024-01-16 15:56:45 +0100

    Not tested

--- src/fat32.h
+++ src/fat32.h
@@ -22,6 +22,11 @@
 #ifndef __FAT32_H__
 #define __FAT32_H__
 
+  // RETURN
+  // ------------------------------------------------------------------
+  #define FAT32_ERROR                   0xff
+  #define FAT32_SUCCESS                 0x00
+
   // Partition Type used in the partition record
   //
   // @src https://d1.amobbs.com/bbs_upload782111/files_2/armok0150242.pdf
@@ -29,12 +34,14 @@
   //      https://www.richud.com/wiki/FAT32_Filesystem_Slice_Design_Card
   //      https://en.wikipedia.org/wiki/Master_boot_record#PTE
   // --------------------------------------------------------------------------------------
+  #define PE_STATUS_ACTIVE_FLAG         0x7F
+
   #define PE_TYPECODE_UNKNOWN           0x00
   #define PE_TYPECODE_FAT12             0x01
   #define PE_TYPECODE_XENIX             0x02
   #define PE_TYPECODE_DOSFAT16          0x04
   #define PE_TYPECODE_EXTDOS            0x05
-  #define PE_TYPECODE_FAT16             0x06 
+  #define PE_TYPECODE_FAT16             0x06
   #define PE_TYPECODE_NTFS              0x07
   #define PE_TYPECODE_FAT32             0x0B
   #define PE_TYPECODE_FAT32LBA          0x0C
@@ -48,36 +55,38 @@
   #define PE_TYPECODE_DBFS              0xE0
   #define PE_TYPECODE_BBT               0xFF
 
-  // MASTER BOOT RECORD
-  typedef struct MBR {
-    uint8_t bootstrap[446];
-    PE partition1;                                      // 16 bytes Partition Entry
-    PE partition2;                                      // 16 bytes Partition Entry
-    PE partition3;                                      // 16 bytes Partition Entry
-    PE partition4;                                      // 16 bytes Partition Entry
-    uint8_t signature[2];                               // signature => must be 0xAA55
-  } __attribute__((packed)) MBR;
-  
-  // PARTITION ENTRY
-  typedef struct PE {
-    uint8_t status[1];                                  // status: 0x80-active, 0x00-inactive
+  #define BYTES_PER_SECTOR              0x0200          // 512 Bytes
+
+  // Partition Entry PE
+  typedef struct s_PE {
+    uint8_t status;                                     // status: 0x80-active, 0x00-inactive
     uint8_t CHSBegin[3];                                // Cylinder/Head/Sector start
-    uint8_t typeCode[1];                                // partition type: 0x0E-FAT16 LBA, 0x0B-FAT32, 0x0C-FAT32 LBA
+    uint8_t typeCode;                                   // partition type: 0x0E-FAT16 LBA, 0x0B-FAT32, 0x0C-FAT32 LBA
     uint8_t CHSEnd[3];                                  // Cylinder/Head/Sector end
     uint8_t LBABegin[4];                                // LBA of first sector
-    uint8_t noOfSectors[4];                             // numbers of sectors in partition
-  } __attribute__((packed)) PE;
+    uint8_t numOfSectors[4];                            // numbers of sectors in partition
+  } __attribute__((packed)) s_PE;
 
-  // BOOT SECTOR
-  typedef struct BS {
+  // Master Boot Record MBR
+  typedef struct s_MBR {
+    uint8_t bootstrap[446];
+    s_PE partition1;                                    // 16 bytes Partition Entry
+    s_PE partition2;                                    // 16 bytes Partition Entry
+    s_PE partition3;                                    // 16 bytes Partition Entry
+    s_PE partition4;                                    // 16 bytes Partition Entry
+    uint8_t signature[2];                               // signature => must be 0xAA55
+  } __attribute__((packed)) s_MBR;
+
+  // Boot Sector (or Volume ID or Block Parameter BIOS)
+  typedef struct s_VID {
     uint8_t jumpCode[3];
     uint8_t oemName[8];
-    uint8_t bytesPerSector[2];
-    uint8_t sectorsPerCluster[1];
-    uint8_t reservedSectors[2];
-    uint8_t numOfFat[1];
+    uint8_t bytesPerSector[2];                          // offset 0x0B
+    uint8_t sectorsPerCluster;                          // offset 0x0D
+    uint8_t reservedSectors[2];                         // offset 0x0E
+    uint8_t numOfFat;                                   // offset 0x10
     uint8_t empty[509];
-    uint8_t signature[2];                               // signature => must be 0xAA55
-  } __attribute__((packed)) BS;
+    uint8_t signature[2];                               // offset 0x1FE - signature => must be 0xAA55
+  } __attribute__((packed)) s_VID;
 
 #endif
