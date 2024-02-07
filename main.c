be34f3b67aa3ad33385b025b329be7a74b9e8e36
Marián <Matiasus@users.noreply.github.com>
2024-02-07 20:43:05 +0100

    Display files in loop

--- main.c
+++ main.c
@@ -32,7 +32,17 @@
 int main (void)
 {
   char str[10];
-  FAT32_t FAT32 = {.lba_begin =0, .fats_begin = 0, .data_begin = 0, .root_begin = 0};
+  DE_t * File;
+  uint8_t files;
+  uint32_t cluster;
+  FAT32_t FAT32 = {
+    .root_dir_clus_num = 0, 
+    .sectors_per_cluster = 0, 
+    .lba_begin = 0, 
+    .fats_begin = 0, 
+    .data_begin = 0, 
+    .root_begin = 0
+  };
   
   // Init LCD SSD1306
   // -------------------------------------------------------------------------------------
@@ -43,26 +53,41 @@ int main (void)
 
   // Init SD
   // ----------------------------------------------------------------
-  if (FAT32_Init() == FAT32_ERROR) {
+  if (FAT32_Init(&FAT32) == FAT32_ERROR) {
     SSD1306_SetPosition (20, 3);
     SSD1306_DrawString ("ERROR", NORMAL);
     return FAT32_ERROR;
   }
 
-  // Read Master Boot Record
+  // Rood Directory Number Of Files
   // ----------------------------------------------------------------
-  FAT32_Read_Master_Boot_Record (&FAT32);
-
-  // Read Boot Sector
-  // ----------------------------------------------------------------
-  FAT32_Read_Boot_Sector (&FAT32);
+  files = FAT32_Root_Dir_Files (&FAT32);
+  SSD1306_SetPosition (0, 2);
+  sprintf (str, "%d", files);
+  SSD1306_DrawString ("SUMs: ", NORMAL);
+  SSD1306_DrawString (str, NORMAL);
 
   // Rood Directory Number Of Files
-  // ---------------------------------------------------------------- 
-  SSD1306_SetPosition (0, 2);
-  sprintf (str, "%ld", FAT32_Root_Dir_Files (&FAT32));
-  SSD1306_DrawString ("Files: ", NORMAL);
-  SSD1306_DrawString (str, NORMAL);
+  // ----------------------------------------------------------------
+  for (uint32_t i = 1; i<7; i++) {
+
+    File = FAT32_Get_File (&FAT32, i);
+    cluster = ((uint32_t) FAT32_Get_2Bytes_LE (File->FirstClustHI) << 16) | FAT32_Get_2Bytes_LE (File->FirstClustLO);
+
+    SSD1306_SetPosition (0, 3);
+    SSD1306_DrawString ("FILE: ", NORMAL);
+    sprintf (str, "%d", (int) i);
+    SSD1306_SetPosition (0, 4);
+    SSD1306_DrawString ("NAME: ", NORMAL);
+    SSD1306_DrawString ((char *) File->Name, NORMAL);
+    SSD1306_SetPosition (0, 5);
+    SSD1306_DrawString ("CLUS: ", NORMAL);
+    sprintf (str, "0x%08x", (unsigned int) cluster);
+    SSD1306_DrawString (str, NORMAL);
+    
+    _delay_ms (2000);
+    //SSD1306_ClearScreen ();
+  }
 
   // EXIT
   // ----------------------------------------------------------------
